026fc9e996b2e9aad029a36eacb5a54397db4a8a
let_chaos <5yees@protonmail.com>
2022-12-17 19:15:20 -0600

    Add player moves

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -2,8 +2,11 @@ import { useFrame, useThree } from '@react-three/fiber'
 import { useSphere } from '@react-three/cannon'
 import { useEffect, useRef } from 'react'
 import { Vector3 } from 'three'
+import { useKeyboard } from '../hooks/useKeyboard'
 
 export const Player = () => {
+  const actions = useKeyboard()
+  console.log(actions)
   const { camera } = useThree()
   const [ ref, api ] = useSphere(() => ({
     mass: 1,
